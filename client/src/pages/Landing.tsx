d2e1611b32d85d8e0ad2ac562d5f9a12710dd34e
Souad-HB <souadsalahh@gmail.com>
2024-12-17 07:15:01 -0500

    adjusted colors

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -16,28 +16,30 @@ function Landing() {
       {/* Left Section for Text Content */}
       <div className="w-1/2 flex flex-col justify-center items-start px-12">
         <div className="absolute top-4 left-4 z-10">
-          <h2 className="text-2xl font-bold text-gray-800 tracking-widest px-7">
-            FWOB
+          <h2 className="font-bold tracking-widest px-7 flex flex-row">
+            <span className="text-teal-950 text-2xl">F</span>
+            <span className="text-teal-900 text-1xl self-end">WO</span>
+            <span className="text-teal-950 text-2xl">B</span>
           </h2>
         </div>
-        <h3 className="text-lg text-gray-600 mb-4 tracking-wider">
+        <h3 className="text-lg text-teal-900 mb-4 tracking-wider">
           Think fast. Spot faster. Win big.
         </h3>
-        <h1 className="text-6xl font-extrabold text-gray-900 mb-6 tracking-wide leading-snug">
+        <h1 className="text-5xl font-extrabold text-teal-950 mb-6 tracking-wide leading-snug">
           FIND OBJECTS USING YOUR CAMERA
         </h1>
-        <h2 className="text-lg text-gray-600 mb-8 tracking-wider">
+        <h2 className="text-lg text-teal-900 mb-8 tracking-wider">
           Turn your everyday items into a fun game of detection!
         </h2>
         <div className="flex flex-row space-x-4">
           <button
-            className="rounded-md bg-blue-500 px-6 py-3 text-white font-semibold shadow hover:bg-blue-600 transition"
+            className="rounded-md bg-teal-950 px-6 py-3 text-white font-semibold shadow hover:bg-teal-900 transition"
             onClick={handleSignUp}
           >
             Get Started
           </button>
           <button
-            className="rounded-md bg-blue-500 px-6 py-3 text-white font-semibold shadow hover:bg-blue-600 transition"
+            className="rounded-md bg-teal-950 px-6 py-3 text-white font-semibold shadow hover:bg-teal-900 transition"
             onClick={handleLogin}
           >
             Sign In
