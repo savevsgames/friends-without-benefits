5c7f379a720e7389919ddf0b732390a5a6fe0835
Souad-HB <souadsalahh@gmail.com>
2024-12-17 19:05:49 -0500

    header ok-ish

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,34 @@
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from "react-router-dom";
 
+import Header from "./components/Header.tsx";
+import Footer from "./components/Footer.tsx";
+import "./index.css";
+import Landing from "./pages/Landing.tsx";
+import Error from "./pages/Error.tsx";
+import Game from "./pages/Game.tsx";
+import LeaderBoard from "./pages/LeaderBoard";
+import Profile from "./pages/Profile.tsx";
 
-// import NavBar from './components/NavBar.tsx';
-// import Footer from './components/Footer.tsx';
-import "./index.css"
-import Landing from './pages/Landing.tsx';
-import Error from './pages/Error.tsx';
-import Game from './pages/Game.tsx';
-import LeaderBoard from './pages/LeaderBoard';
-import Profile from './pages/Profile.tsx';
-
-
-function Layout () {
+function Layout() {
   return (
     <div>
-   
-      <Outlet />
-
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
-)}
+  );
+}
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Landing />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="/leaderboard" element={<LeaderBoard />} />
+      <Route path="/" element={<Landing />} />
+      <Route element={<Layout />}>
         <Route path="/profile" element={<Profile />} />
+        <Route path="/leaderboard" element={<LeaderBoard />} />
+        <Route path="/game" element={<Game />} />
         <Route path="*" element={<Error />} />
       </Route>
     </Routes>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,10 +3,17 @@ export default {
   content: [
     "./src/**/*.{html,js,jsx,ts,tsx}",
     "./components/**/*.{html,js,jsx,ts,tsx}",
-    "./index.html"
+    "./index.html",
   ],
   theme: {
     extend: {},
+    theme: {
+      colors: {
+        color0: "#052e16", //teal
+        color1: "#14532d", // light teal
+        color2: "#f1f5f9", // light gray
+      },
+    },
   },
   plugins: [],
 };
