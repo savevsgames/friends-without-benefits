329e9931603d6f510209a4a9139b7d976490eef1
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-12-20 07:26:04 -0700

    Updated vite config and using vite static assets to build and copy the ML .js files into dist, added scripts to index.html and tested. Works until event listeners attach - needs integration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths" //chakra
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths"; //chakra
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./src/utils/*.js",
+          dest: "", // Destination folder within `dist` - root
+        },
+      ],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      external: ["opencv.js", "main.js"], // List files or libraries to exclude from bundling
+    },
+  },
+});
