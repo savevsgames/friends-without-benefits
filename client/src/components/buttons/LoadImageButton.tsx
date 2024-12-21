9e8985aaea83e77a3f3ef5d3fb4dde59353e3d9e
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-12-21 02:36:50 -0700

    Working on including input element event change for button to load images

--- client/src/components/buttons/LoadImageButton.tsx
+++ client/src/components/buttons/LoadImageButton.tsx
@@ -1,26 +1,49 @@
 import { loadImageToCanvas } from "@/utils/utils";
+import { useRef } from "react";
+import { useGameStore } from "@/store";
+
+const setCurrentImageRef = useGameStore((state) => state.setCurrentImageRef);
 
 const handleLoadImage = async (file: File) => {
   await loadImageToCanvas(file);
 };
 
+const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  const file = e.target.files?.[0];
+  if (file) {
+    setCurrentImageRef(file.name);
+    await handleLoadImage(file);
+  }
+};
+
 const LoadImageButton = () => {
   return (
-    <button
-      className="btn btn-primary"
-      type="button"
-      id="load-image"
-      name="load-image"
-      onClick={(e) => {
-        // Load image from file input
-        const input = e.target as HTMLInputElement;
-        if (input.files && input.files[0]) {
-          handleLoadImage(input.files[0]);
-        }
-      }}
-    >
-      Load Image
-    </button>
+    <div>
+      <button
+        className="btn btn-primary"
+        type="button"
+        id="load-image"
+        name="load-image"
+        onClick={(e) => {
+          // Load image from file input
+          const input = e.target as HTMLInputElement;
+          if (input.files && input.files[0]) {
+            handleLoadImage(input.files[0]);
+          }
+        }}
+      >
+        Load Image
+      </button>
+      <input
+        ref="{fileInputRef}"
+        type="file"
+        accept="image/*"
+        id="file-input"
+        name="file-input"
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+    </div>
   );
 };
 export default LoadImageButton;
