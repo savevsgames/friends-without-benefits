f0092225858626ba4195ad84d692bd072edfd981
Souad-HB <souadsalahh@gmail.com>
2024-12-21 19:57:02 -0500

    adjusted better routing between app and main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,6 @@
-import { Outlet, Route, Routes } from "react-router-dom";
-
-import Header from "./components/Header.tsx";
-// import Footer from "./components/Footer.tsx";
+import { Outlet } from "react-router-dom";
 import "./index.css";
-import Landing from "./pages/Landing.tsx";
-import Error from "./pages/Error.tsx";
-import Game from "./pages/Game.tsx";
-import LeaderBoard from "./pages/LeaderBoard";
-import Profile from "./pages/Profile.tsx";
-import Login from "./pages/Login.tsx";
+
 import {
   ApolloProvider, 
   ApolloClient,
@@ -33,11 +25,10 @@ const client = new ApolloClient({
 });
 
 
-function Layout() {
+function App() {
   return (
     <ApolloProvider client={client}>
       <>
-        <Header />
         <main>
           <Outlet />
         </main>
@@ -46,20 +37,20 @@ function Layout() {
   );
 }
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/login" element={<Login />} />
-      <Route element={<Layout />}>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/leaderboard" element={<LeaderBoard />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="*" element={<Error />} />
-      </Route>
-    </Routes>
-  );
-}
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Landing />} />
+//       <Route path="/login" element={<Login />} />
+//       <Route element={<Layout />}>
+//         <Route path="/profile" element={<Profile />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/leaderboard" element={<LeaderBoard />} />
+//         <Route path="/game" element={<Game />} />
+//         <Route path="*" element={<Error />} />
+//       </Route>
+//     </Routes>
+//   );
+// }
 
 export default App;
