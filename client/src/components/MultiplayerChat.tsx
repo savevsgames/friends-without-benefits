790d70d55906b8f1f953b53568f2fa82b8b2aee2
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-12-27 23:53:16 -0700

    Working on MultiplayerChat.tsx - return point if errors occur

--- client/src/components/MultiplayerChat.tsx
+++ client/src/components/MultiplayerChat.tsx
@@ -2,6 +2,22 @@ import React, { useState, useEffect } from "react";
 import { useMultiplayerStore } from "@/store";
 
 const MultiplayerChat = () => {
+  const { socket, isConnected, chatMessages, addChatMessage, playerId } =
+    useMultiplayerStore();
+
+  // For local chat message state on react / DOM component
+  const [message, setMessage] = useState<string>("");
+
+  // Handle incoming socket.io chat messages
+  useEffect(() => {}, [socket, addChatMessage]);
+
+  // Send message to the socket.io server
+  const sendMessage = () => {};
+
+  if (!isConnected) {
+    return <div>Connecting...</div>;
+  }
+
   return <div>MultiplayerChat</div>;
 };
 export default MultiplayerChat;
