04ddf128824842c8480471ee8168bb39705fd392
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-12-28 14:39:14 -0700

    Setting up the multiplayer video feed component

--- client/src/components/MultiplayerVideoFeed.tsx
+++ client/src/components/MultiplayerVideoFeed.tsx
@@ -1,4 +1,28 @@
-const MultiplayerVideoFeed = () => {
-  return <div>MultiplayerVideoFeed</div>;
+import React, { useEffect, useState } from "react";
+// import { useGameStore } from "@/store";
+import { useMultiplayerStore } from "@/store";
+import { enableWebcam } from "@/utils/model-utils";
+
+const MultiplayerVideoFeed: React.FC = () => {
+  const {
+    peer,
+    setPeer,
+    socket,
+    isConnected,
+    setIsConnected,
+    playerId,
+    isHost,
+  } = useMultiplayerStore();
+  return (
+    <div id="challenger-video-wrapper">
+      <video
+        ref={localVideoRef}
+        id="challenger-video-output"
+        autoPlay
+        muted
+        style={{ width: "300px", border: "1px solid black" }}
+      />
+    </div>
+  );
 };
 export default MultiplayerVideoFeed;
