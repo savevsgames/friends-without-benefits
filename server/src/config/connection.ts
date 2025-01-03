a90d3a29c9ea72bfffdcb79bea67b6f213731460
savevsgames <126988787+savevsgames@users.noreply.github.com>
2025-01-03 15:49:44 -0700

    updated the connection.ts to not fail in staging env

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-const MONGODB_URI_MPTESTING = process.env.MONGODB_URI_MPTESTING;
+const MONGODB_URI = process.env.MONGODB_URI || "";
+const MONGODB_URI_MPTESTING = process.env.MONGODB_URI_MPTESTING || "";
 
 const db = async (): Promise<typeof mongoose.connection> => {
   if (!MONGODB_URI || !MONGODB_URI_MPTESTING) {
